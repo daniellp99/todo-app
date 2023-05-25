3b3c2e6ff551ae8a3e16e7ff1525a43b66b4d727
Daniel Alejandro Leyva Perez <dalejandrolp99@gmail.com>
2023-05-25 10:30:50 -0400

    feat: add header home page and new page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-slate-800 text-slate-100 container mx-auto p-4`}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <h1>Todo</h1>;
+import Link from "next/link";
+
+export default async function Home() {
+  return (
+    <>
+      <header className="flex items-center justify-between mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/new"
+        >
+          New
+        </Link>
+      </header>
+    </>
+  );
 }
