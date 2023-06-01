3a4b0dbeebdb03d38286272593460590bfd266bb
Daniel Alejandro Leyva Perez <dalejandrolp99@gmail.com>
2023-06-01 16:06:44 -0400

    feat: add create todo functionality

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,46 @@
+import { prisma } from "@/db";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+
+async function createTodo(data: FormData) {
+  "use server";
+
+  const title = data.get("title")?.valueOf();
+  if (typeof title !== "string" || title.length === 0) {
+    throw new Error("Invalid Title");
+  }
+  await prisma.todo.create({ data: { title } });
+  redirect("/");
+}
+
 export default function NewPage() {
-  return <div>NewPage</div>;
+  return (
+    <>
+      <header className="flex items-center justify-between mb-4">
+        <h1 className="text-2xl">New</h1>
+      </header>
+      <form action={createTodo} className="flex flex-col gap-2">
+        <input
+          type="text"
+          name="title"
+          id="title"
+          className="border border-slate-300 text-slate-300 px-2 py-1 focus-within:border-slate-100 bg-transparent rounded outline-none"
+        />
+        <div className="flex gap-1 justify-end">
+          <Link
+            href=".."
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          >
+            Create
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
